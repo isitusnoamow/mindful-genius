11bc1592379d12eb9baba919b6579a4242a83c10
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-08-06 13:57:48 +0800

    add gpt and tts generation

---
+++ meditation-generation/tts/generateTts.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { config } from 'dotenv';  
+
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = dirname(__filename);
+
+config({ path: `${__dirname}/../.env` });
+
+export const generateTextToSpeech = async (inputText) => {
+  const API_KEY = process.env.ELEVENLABS_API_KEY;
+  const VOICE_ID = 'VR6AewLTigWG4xSOukaG';
+
+  const options = {
+    method: 'POST',
+    url: `https://api.elevenlabs.io/v1/text-to-speech/${VOICE_ID}`,
+    headers: {
+      accept: 'audio/mpeg', 
+      'content-type': 'application/json', 
+      'xi-api-key': `${API_KEY}`,
+    },
+    data: {
+      text: inputText, 
+    },
+    responseType: 'arraybuffer', 
+  };
+
+  const speechDetails = await axios.request(options);
+
+  return speechDetails.data;
+};
